3d6b9d0602216bb61ae50c82e985609c59d45267
Benjamin <Benjamin@192.168.0.192>
2017-02-13 22:12:28 +0100

    Finally made an animation

--- Game/src/ord/boka/game/Main.java
+++ Game/src/ord/boka/game/Main.java
@@ -1,25 +1,50 @@
 package ord.boka.game;
 
-import java.awt.Color;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.JPanel;
 
-public class Main{
-	
+public class Main extends JPanel{
+
 		final static String NAMEOFGAME = "Rectangle Escape";
-		final static int WIDTH = 1000; final static int HEIGHT = WIDTH;
-	
-		static JFrame openWindow(int x, int y, String title){
-			JFrame myFrame = new JFrame(title);
-	        myFrame.setSize(x,y);
-	        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	        myFrame.setVisible(true);
-	        return myFrame;
+		final static int WIDTH = 500, HEIGHT = 500;
+		final static int BALLDIAMETER = 20;
+		private int ballX = WIDTH/2-BALLDIAMETER/2,ballY= HEIGHT/2-BALLDIAMETER/2;
+		
+		public Main(){
+			setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		}
-	public static void main(String[] args){
-		JFrame myFrame = new JFrame();
-		myFrame = Main.openWindow(WIDTH,HEIGHT,NAMEOFGAME);
-		myFrame.getContentPane().setBackground(Color.GRAY);
+		
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.fillOval(ballX, ballY, BALLDIAMETER, BALLDIAMETER);
+		}
+		
+		public void moveBall(){
+			ballX++;
+			ballY++;
+		}
+
+	public static void main(String[] args) throws InterruptedException{
+		JFrame frame = new JFrame("NAMEOFGAME");
+		Main game = new Main();
+		frame.add(game);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while(true){
+			game.moveBall();
+			game.repaint();
+			Thread.sleep(10);
+		}
 	}
 }
