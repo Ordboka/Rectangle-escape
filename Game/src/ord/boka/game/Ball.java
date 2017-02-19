e18900feaba10d3db6646075c643871f71d36af5
Benjamin <Benjamin@192.168.0.192>
2017-02-19 11:33:36 +0100

    Added support for multiple balls.

---
+++ Game/src/ord/boka/game/Ball.java
@@ -0,0 +1,79 @@
+package ord.boka.game;
+
+import java.awt.Color;
+
+public class Ball {
+	
+	private int up, down, left, right;
+	private Color color;
+	public final static int BALLDIAMETER = 20;
+	private int ballX = Main.WIDTH/2-BALLDIAMETER/2,ballY= Main.HEIGHT/2-BALLDIAMETER/2;
+	private int ballXSpeed = 0, ballYSpeed= 0;
+	private double speedIncrease = 2;
+	
+	/**
+	 * Constructor for Ball where the keys used for moving the ball is given together with the color used.
+	 * @param up The key that is used for moving up
+	 * @param down The key that is used for moving down
+	 * @param left The key that is used for moving left
+	 * @param right The key that is used for moving right
+	 * @param color The color of the ball
+	 */
+	public Ball(int up, int down, int left, int right, Color color){
+		this.up = up;
+		this.down = down;
+		this.left = left;
+		this.right = right;
+		this.color = color;
+	}
+	public void moveBall(){
+		ballX+=ballXSpeed;
+		ballY+=ballYSpeed;
+	}
+	public int getUp() {
+		return up;
+	}
+	public int getDown() {
+		return down;
+	}
+	public int getLeft() {
+		return left;
+	}
+	public int getRight() {
+		return right;
+	}
+	public int getX(){
+		return ballX;
+	}
+	public int getY(){
+		return ballY;
+	}
+	public Color getColor(){
+		return color;
+	}
+	
+	public int getBallXSpeed() {
+		return ballXSpeed;
+	}
+	public int getBallYSpeed() {
+		return ballYSpeed;
+	}
+	public void upPressed(){
+		ballYSpeed-=speedIncrease;
+	}
+	public void downPressed(){
+		ballYSpeed+=speedIncrease;
+	}
+	public void yReleased(){
+		ballYSpeed = 0;
+	}
+	public void leftPressed(){
+		ballXSpeed-=speedIncrease;
+	}
+	public void rightPressed(){
+		ballXSpeed+=speedIncrease;
+	}
+	public void xReleased(){
+		ballXSpeed = 0;
+	}
+}
--- Game/src/ord/boka/game/Main.java
+++ Game/src/ord/boka/game/Main.java
@@ -1,23 +1,23 @@
 package ord.boka.game;
 
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JPanel{
 
-		final static String NAMEOFGAME = "Rectangle Escape";
-		final static int WIDTH = 500, HEIGHT = 500;
-		final static int BALLDIAMETER = 20;
-		private int ballX = WIDTH/2-BALLDIAMETER/2,ballY= HEIGHT/2-BALLDIAMETER/2;
-		private int ballXSpeed = 0, ballYSpeed= 0;
+		public final static String NAMEOFGAME = "Rectangle Escape";
+		public final static int WIDTH = 500, HEIGHT = 500;
+		private ArrayList<Ball> balls = new ArrayList<Ball>();
 		
 		public Main(){
 			setPreferredSize(new Dimension(WIDTH,HEIGHT));
@@ -32,12 +32,18 @@ public class Main extends JPanel{
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 					RenderingHints.VALUE_ANTIALIAS_ON);
-			g2d.fillOval(ballX, ballY, BALLDIAMETER, BALLDIAMETER);
+			for(Ball ball: balls){
+				g2d.setColor(ball.getColor());
+				g2d.fillOval(ball.getX(), ball.getY(), Ball.BALLDIAMETER, Ball.BALLDIAMETER);
+			}
+			
 		}
 		
 		public void moveBall(){
-			ballX+=ballXSpeed;
-			ballY+=ballYSpeed;
+			for(Ball ball: balls){
+				ball.moveBall();
+			}
+			
 		}
 
 	public static void main(String[] args) throws InterruptedException{
@@ -47,6 +53,10 @@ public class Main extends JPanel{
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Ball ball1 = new Ball(38, 40, 37, 39, Color.PINK);
+		game.balls.add(ball1);
+		Ball ball2 = new Ball(87, 83, 65, 68, Color.CYAN);
+		game.balls.add(ball2);
 		while(true){
 			game.moveBall();
 			game.repaint();
@@ -60,51 +70,33 @@ public class Main extends JPanel{
 
 		@Override
 		public void keyPressed(KeyEvent e) {
-			switch (KeyEvent.getKeyText(e.getKeyCode())){
-			case "W":
-			case "Up":
-				if(ballYSpeed==0){
-				ballYSpeed-=1;}
-				break;
-			case "S":
-			case "Down":
-				if(ballYSpeed==0){
-					ballYSpeed+=1;}
-				break;
-			case "A":
-			case "Left":
-				if(ballXSpeed==0){
-					ballXSpeed-=1;}
-				break;
-			case "D":
-			case "Right":
-				if(ballXSpeed==0){
-					ballXSpeed+=1;}
-				break;
-			}
+			for(Ball ball: balls){
+				//Sjekker hvilken knapp som ble trykket, og om den allerede beveger seg i denne retningen.
+				//hvis dette er tilfelle vil ikke bevegelsen endre seg
+				if(e.getKeyCode()==ball.getUp() && ball.getBallYSpeed()==0){
+					System.out.println(ball.getUp());
+					ball.upPressed();
+				}else if(e.getKeyCode()==ball.getDown() && ball.getBallYSpeed()==0){
+					ball.downPressed();
+				}else if(e.getKeyCode()==ball.getLeft() && ball.getBallXSpeed()==0){
+					ball.leftPressed();
+				}else if(e.getKeyCode()==ball.getRight() && ball.getBallXSpeed()==0){
+					ball.rightPressed();
+				}
 				
-			System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+			}
+			System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode())+ ". The code is " + e.getKeyCode());
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			switch (KeyEvent.getKeyText(e.getKeyCode())){
-			case "W":
-			case "Up":
-				ballYSpeed=0;
-				break;
-			case "S":
-			case "Down":
-				ballYSpeed=0;
-				break;
-			case "A":
-			case "Left":
-				ballXSpeed=0;
-				break;
-			case "D":
-			case "Right":
-				ballXSpeed=0;
-				break;
+			for(Ball ball: balls){
+				//Stopper bevegelsen når knappen er sluppet
+				if(e.getKeyCode()==ball.getUp() || e.getKeyCode()==ball.getDown()){
+					ball.yReleased();
+				}else if(e.getKeyCode()==ball.getLeft() || e.getKeyCode()==ball.getRight()){
+					ball.xReleased();
+				}
 			}
 			System.out.println("keyReleased="+KeyEvent.getKeyText(e.getKeyCode()));
 		}
